19fc34706c7bc3f4b7befef551d4156dab86c4c6
Or Bartal <orbartal@gmail.com>
2023-05-03 14:03:35 -0400

    Replace ConfigProvider with TargetProptiesProvider in FileClientApi.

--- tester/src/main/java/demo/springboot/files/storage/tester/files/helper/api/FileClientApi.java
+++ tester/src/main/java/demo/springboot/files/storage/tester/files/helper/api/FileClientApi.java
@@ -2,34 +2,31 @@ package demo.springboot.files.storage.tester.files.helper.api;
 
 import java.io.File;
 
-import demo.springboot.files.storage.tester.files.helper.config.ConfigProvider;
-import demo.springboot.files.storage.tester.files.helper.config.TestConfig;
+import demo.springboot.files.storage.tester.config.target.TargetPropties;
+import demo.springboot.files.storage.tester.config.target.TargetProptiesProvider;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 
 public class FileClientApi {
 
-	private static final TestConfig CONFIG = ConfigProvider.get();
-	private static final String TARGET_BASE_URL = CONFIG.getServerBaseUrl();
-	private static final String API_UPLOAD_PATH = CONFIG.getApiUploadFile();
-	private static final String API_DOWNLOAD_PATH = CONFIG.getApiDownloadFile();
+	private final TargetPropties target = TargetProptiesProvider.getTargetPropties();
 
 	private static final String MULTIPART_FORM_DATA_VALUE = "multipart/form-data";
 
 	public FileClientApi() {
-		RestAssured.baseURI = TARGET_BASE_URL;
+		RestAssured.baseURI = target.getTargetBseUrl();
 	}
 
 	public Response uploadFile(File file) {
 		return RestAssured.given()
 				.contentType(MULTIPART_FORM_DATA_VALUE)
 				.multiPart("file", file)
-				.request(Method.POST, API_UPLOAD_PATH);
+				.request(Method.POST, target.getTargetApiUpload());
 	}
 
 	public Response downloadFile(String uid) {
-		String url = API_DOWNLOAD_PATH + "/" + uid;
+		String url = target.getTargetApiDownload() + "/" + uid;
 		return RestAssured.given().when().get(url).andReturn();
 	}
 
